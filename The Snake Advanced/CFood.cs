9ad04e8807fecdf817b896bb95443bf6ed44861c
mohammad mazaheri <53862625+xmsa@users.noreply.github.com>
2019-11-23 01:28:59 +0330

    CreateFood and RandomLocation CFood

--- The Snake Advanced/CFood.cs
+++ The Snake Advanced/CFood.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace The_Snake_Advanced
 {
@@ -13,9 +14,35 @@ namespace The_Snake_Advanced
     /// 
     class CFood
     {
+        FrmMain frmMain;
+        Size size { get; set; }
+        public bool sFood { get; set; }
+        public PictureBox food { get; set; }
+
         public CFood(FrmMain frmMain, Size size, bool sFood = false)
         {
+            this.frmMain = frmMain;
+            this.size = size;
+            this.sFood = sFood;
 
+            CreateFood();
+        }
+
+        void CreateFood()
+        {
+            food = new PictureBox();
+            food.Size = size;
+
+            food.BackColor = Color.Red;
+            RandomLocation();
+            frmMain.Controls.Add(food);
+        }
+        public void RandomLocation()
+        {
+            Random rnd = new Random();
+            int width = rnd.Next(1, (frmMain.Width / size.Width) - 1);
+            int height = rnd.Next(1, ((frmMain.Height - 24) / size.Height) - 1);
+            food.Location = new Point(width * size.Width, height * size.Height);
         }
     }
 }
