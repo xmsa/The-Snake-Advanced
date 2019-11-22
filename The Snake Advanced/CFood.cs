2a2ee27501efe601047352fd42ca9d037fe6de35
mohammad mazaheri <53862625+xmsa@users.noreply.github.com>
2019-11-23 00:45:06 +0330

    Create class CFood

---
+++ The Snake Advanced/CFood.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Snake_Advanced
+{
+    /// <summary>
+    /// Create food and shift location
+    /// </summary>
+    /// 
+    class CFood
+    {
+        public CFood(FrmMain frmMain, Size size, bool sFood = false)
+        {
+
+        }
+    }
+}
