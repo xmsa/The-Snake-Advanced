4e69375d23280be55ba3dda5e20d0ae10c7416ea
mohammad mazaheri <53862625+xmsa@users.noreply.github.com>
2019-11-23 00:40:05 +0330

    Create calss CSnake

---
+++ The Snake Advanced/CSnake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace The_Snake_Advanced
+{
+
+    /// <summary>
+    ///Add / Move Body and Check eatting Food and Check Law
+    /// </summary>
+    ///
+    class CSnake
+    {
+        public CSnake(FrmMain frmMain, Point locationStart, Size size, Color color)
+        {
+
+        }
+    }
+}
