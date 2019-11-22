0c1a63b11df087fb6f864b6254329ac2dc11185e
mohammad mazaheri <53862625+xmsa@users.noreply.github.com>
2019-11-23 03:15:17 +0330

    Add body and laws

--- The Snake Advanced/CSnake.cs
+++ The Snake Advanced/CSnake.cs
@@ -15,9 +15,168 @@ namespace The_Snake_Advanced
     ///
     class CSnake
     {
-        public CSnake(FrmMain frmMain, Point locationStart, Size size, Color color)
+        FrmMain frmMain { get; set; }
+        Point _locationHead;
+        public Point locationHead { get { return _locationHead; } }
+        Size size { get; set; }
+        Color color { get; set; }
+        List<PictureBox> body { get; set; }
+        Keys key;
+
+        public CSnake(FrmMain frmMain, Point locationStart, Size size, Color color, Keys key)
+        {
+            this.frmMain = frmMain;
+            this._locationHead = locationStart;
+            this.size = size;
+            this.color = color;
+            this.key = key;
+            body = new List<PictureBox>();
+            AddBody();
+        }
+
+        private void AddBody()
         {
 
+            _locationHead = SetLocationHead();
+
+            LowWall(_locationHead);
+
+            if (frmMain.gameOver)
+            {
+                return;
+            }
+
+            PictureBox tail = new PictureBox();
+            tail.Size = size;
+            tail.BackColor = Color.Red;
+            tail.Location = locationHead;
+            if (body.Count>0)
+            {
+                body[body.Count - 1].BackColor = color;
+            }
+            body.Add(tail);
+            frmMain.Controls.Add(tail);
+        }
+
+        private Point SetLocationHead()
+        {
+            Point location = locationHead;
+
+            switch (frmMain.law)
+            {
+                case FrmMain.laws.Overfly:
+                    location = LowOverfly(location);
+                    break;
+                case FrmMain.laws.CuttingSnake:
+                    location = LowCuttingSnake(location);
+                    break;
+                case FrmMain.laws.NoCuttingSnake:
+                    location = LowNoCuttingSnake(location);
+                    break;
+                case FrmMain.laws.ReturnOnSnake:
+                    location = LowReturnOnSnake(location);
+                    break;
+            }
+            return location;
+        }
+
+        private Point LowReturnOnSnake(Point location)
+        {
+            location = LowOverfly(location);
+            if ((location.X < 0) ||
+                (location.Y < frmMain.Height) ||
+                (location.X < frmMain.Width)||
+                (location.Y < 0))
+            {
+                key = SwitchKey(key);
+            }
+            return LowOverfly(location);
+        }
+
+        private Keys SwitchKey(Keys key)
+        {
+            switch (key)
+            {
+                case Keys.Up:
+                    return Keys.Down;
+                case Keys.Down:
+                    return Keys.Up;
+                case Keys.Left:
+                    return Keys.Right;
+                case Keys.Right:
+                    return Keys.Left;
+                default:
+                    return key;
+            }
+        }
+
+        private Point LowNoCuttingSnake(Point location)
+        {
+            location = LowOverfly(location);
+            int indexBody= Colision(location);
+            if (indexBody<0)
+            {
+                frmMain.gameOver = true;
+            }
+            return location;
+        }
+
+        private int Colision(Point location)
+        {
+            for (int i = body.Count - 2; i >= 0; i--)
+            {
+                if (location== body[i].Location)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void LowWall(Point location)
+        {
+            if ((location.X < 0) ||
+                (location.Y > frmMain.Height) ||
+                (location.X > frmMain.Width) ||
+                (location.Y < 0))
+            {
+                frmMain.gameOver=true;
+            }
+        }
+
+        private Point LowCuttingSnake(Point location)
+        {
+            location = LowOverfly(location);
+            int indexBody = Colision(location);
+            if (indexBody < 0)
+            {
+                for (int i = indexBody; i >=0; i++)
+                {
+                    frmMain.Controls.Remove(body[i]);
+                    body.RemoveAt(i);
+                }
+            }
+            return location;
+        }
+
+        private Point LowOverfly(Point location)
+        {
+            switch (key)
+            {
+                case Keys.Up:
+                    location.Y -= size.Height;
+                    break;
+                case Keys.Down:
+                    location.Y += size.Height;
+                    break;
+                case Keys.Left:
+                    location.X -= size.Width;
+                    break;
+                case Keys.Right:
+                    location.X += size.Width;
+                    break;
+            }
+            return location;
         }
     }
 }
--- The Snake Advanced/frmMain.cs
+++ The Snake Advanced/frmMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace The_Snake_Advanced
@@ -6,6 +7,10 @@ namespace The_Snake_Advanced
     public partial class FrmMain : Form
     {
         FrmHome frmHome;
+        public enum laws { Overfly, CuttingSnake, NoCuttingSnake, ReturnOnSnake }
+        public laws law { get; set; }
+        public bool gameOver { get; set; }
+
         //Constructor Form frmMain
         public FrmMain(FrmHome frmHome)
         {
@@ -14,6 +19,8 @@ namespace The_Snake_Advanced
             this.Focus();
             frmHome.playAgain = false;
             this.frmHome = frmHome;
+            gameOver = false;
+            CSnake s = new CSnake(this, new Point(50, 50), new Size(20, 20), Color.Black, Keys.Right);
         }
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
